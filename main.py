e2af6e6b41bfcbd2b477535a77fa8e2a02d75aca
Oleksandr Shevchenko <oleksandrs@sapling-inc.com>
2023-02-06 10:28:14 +0200

    Add LogObserver.
    Signed-off-by: Oleksandr Shevchenko <oleksandrs@sapling-inc.com>

--- main.py
+++ main.py
@@ -4,7 +4,18 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from statemachine import StateMachine, State
+from statemachine.exceptions import TransitionNotAllowed
 from statemachine.contrib.diagram import DotGraphMachine
+
+class LogObserver(object):
+    def __init__(self, name):
+        self.name = name
+
+    def after_transition(self, event, source, target):
+        print(f"{self.name} after: {source.id}--({event})-->{target.id}")
+
+    def on_enter_state(self, target, event):
+        print(f"{self.name} enter: {target.id} from {event}")
 
 
 class AlarmMonitor(StateMachine):
@@ -18,36 +29,37 @@ class AlarmMonitor(StateMachine):
     ev_stop = running.to(idle) | alarm.to(idle)
     ev_start = idle.to(running)
     ev_reset = running.to.itself()
-    ev_tick = running.to(alarm, cond="timer_expired") | \
-              alarm.to(running, cond=["timer_expired", "ack"])
+    ev_tick = running.to(alarm, cond=["timer_expired", "alarm_on_timeout"]) | \
+              alarm.to(running, cond=["timer_expired", "auto_ack"])
     ev_alarm = running.to(alarm)
 
-    def __init__(self, name, ack, ack_timeout):
+    def __init__(self, name, alarm_on_timeout=True, auto_ack=False, auto_ack_delay=0):
         self.name = name
-        self.ack = ack
-        self.ack_timeout = ack_timeout
+        self.auto_ack = auto_ack
+        self.alarm_on_timeout = alarm_on_timeout
+        self.auto_ack_delay = auto_ack_delay
         self.calls = []
         super().__init__()
 
-    def before_ev_start(self, event: str, source: State, target: State, message: str = ""):
-        message = ". " + message if message else ""
-        return f"Running {event} from {source.id} to {target.id}{message}"
-
-    def before_ev_tick(self, event: str, source: State, target: State, message: str = ""):
+    def on_ev_tick(self, event: str, source: State, target: State, message: str = ""):
         message = ". " + message if message else ""
         if source.id == "alarm" and target.id == "running":
-            print("Send Alarm Clear message")
+            print("TODO: Send Alarm Clear message")
         return f"Running {event} from {source.id} to {target.id}{message}"
 
     def on_enter_running(self):
         self.timer = self.ALARM_TIMEOUT
 
     def on_enter_alarm(self):
-        self.timer = self.ack_timeout
-        print("Send Alarm message")
+        self.timer = self.auto_ack_delay
+        print("TODO: Send Alarm message")
 
-    def ack(self):
-        return self.ack
+    def auto_ack(self):
+        return self.auto_ack
+
+    def alarm_on_timeout(self):
+        return self.alarm_on_timeout
+
     def timer_expired(self):
         ret = False
         if self.timer != 0:
@@ -79,36 +91,32 @@ class TrafficLightMachine(StateMachine):
         print("Go ahead!")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    traffic_light = TrafficLightMachine()
-    graph = DotGraphMachine(TrafficLightMachine)
-    dot = graph()
-    dot.write_png("TrafficLightMachine_initial.png")
-    print(dot.to_string())
-    traffic_light.cycle()
-    print(traffic_light.cycle())
-    traffic_light.current_state
+    # print_hi('PyCharm')
+    # traffic_light = TrafficLightMachine()
+    # graph = DotGraphMachine(TrafficLightMachine)
+    # dot = graph()
+    # dot.write_png("TrafficLightMachine_initial.png")
+    # print(dot.to_string())
+    # traffic_light.cycle()
+    # print(traffic_light.cycle())
+    # traffic_light.current_state
 
-    uart_alarm_monitor = AlarmMonitor("uart", True, 5)
-    print(uart_alarm_monitor.ev_start())
+    uart_alarm_monitor = AlarmMonitor("uart", auto_ack=True, auto_ack_delay=5)
+    uart_alarm_monitor.add_observer(LogObserver("uart"))
+    uart_alarm_monitor.ev_start()
 
     for i in range(15):
         try:
             if i == 3:
-                print(uart_alarm_monitor.ev_reset())
+                uart_alarm_monitor.ev_reset()
 
             print(uart_alarm_monitor.timer)
-            print(uart_alarm_monitor.ev_tick())
+            uart_alarm_monitor.ev_tick()
 
-
-        except Exception:
+        except TransitionNotAllowed:
             pass
 
     graph = DotGraphMachine(AlarmMonitor)
